df7c4b9f9d026a53b615f6bc7291721cc5c19a38
Maryna <maryny.khrystyianyn@gmail.com>
2026-01-04 12:32:58 +0100

    try 4 for deploy

--- project/app/cheese-batches/add/page.tsx
+++ project/app/cheese-batches/add/page.tsx
@@ -1,7 +1,12 @@
 "use client";  // це зробить всю сторінку client component
 
 import CreateBatchForm from "./CreateBatchForm";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <CreateBatchForm />;
+  return (
+    <Suspense fallback={<p>Lädt…</p>}>
+      <CreateBatchForm />
+    </Suspense>
+  );
 }
